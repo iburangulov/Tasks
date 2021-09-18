05954a72cb7f66b6507361014eecc9a003c19f39
iburangulov <iburanguloff@ya.ru>
2021-09-19 01:45:27 +0500

    tasks front

--- resources/js/components/views/tasks.vue
+++ resources/js/components/views/tasks.vue
@@ -1,10 +1,38 @@
 <template>
-    <h1>Tasks{{ typeof (this.$route.meta.show_top_menu) }}</h1>
+    <div>
+        <button @click="reload">Reload</button>
+        <div v-if="!tasks_loaded && !errors">Loading...</div>
+        <div v-else-if="errors">Error!</div>
+        <div v-else-if="tasks.length === 0">No tasks!</div>
+        <div v-else>
+            <h1>Tasks</h1>
+            <ul v-for="task in tasks" :key="task.id">
+                {{ task }}
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
-    name: "tasks"
+    name: "tasks",
+    mounted() {
+        this.$store.dispatch('load_tasks');
+    },
+    computed: {
+        ...mapGetters({
+            tasks: 'tasks',
+            tasks_loaded: 'tasks_loaded',
+            errors: 'tasks_error'
+        }),
+    },
+    methods: {
+        reload: function () {
+            this.$store.dispatch('load_tasks');
+        }
+    }
 }
 </script>
 
--- resources/js/store/store.js
+++ resources/js/store/store.js
@@ -4,8 +4,50 @@ import Vuex from "vuex"
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {},
-    getters: {}
+    state: {
+        application: {
+            tasks: {
+                loaded: false,
+                data: [],
+                error: false
+            }
+        }
+    },
+    mutations: {
+        set_tasks(state, data) {
+            state.application.tasks.data = data;
+        },
+        set_tasks_loading_status(state, loaded) {
+            state.application.tasks.loaded = Boolean(loaded)
+        },
+        set_tasks_loading_error(state, error) {
+            state.application.tasks.error = Boolean(error)
+        }
+    },
+    actions: {
+        async load_tasks(context) {
+            context.commit('set_tasks_loading_error', false);
+            context.commit('set_tasks_loading_status', false);
+            try {
+                const tasks_data = await axios.get('/api/v1/tasks');
+                context.commit('set_tasks', tasks_data.data);
+                context.commit('set_tasks_loading_status', true);
+            } catch (e) {
+                context.commit('set_tasks_loading_status', true);
+                context.commit('set_tasks_loading_error', true)
+            }
+
+        }
+    },
+    getters: {
+        tasks: state => {
+            return state.application.tasks.data
+        },
+        tasks_error: state => {
+            return state.application.tasks.error
+        },
+        tasks_loaded: state => {
+            return state.application.tasks.loaded
+        },
+    }
 });
