28c7938617d805538b7526a9bffcfb0ae5af5757
iburangulov <iburanguloff@ya.ru>
2021-09-21 03:28:50 +0500

    alert

---
+++ resources/js/components/gui/uiAlert.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        alert
+    </div>
+
+</template>
+
+<script>
+import uiButton from "./uiButton";
+
+export default {
+    name: "uiAlert",
+    components: {
+        uiButton,
+    }
+}
+</script>
+
+<style scoped lang="sass">
+.ui-alert
+    position: fixed
+    z-index: 1000
+    top: 40%
+    left: 50%
+    transform: translateX(-50%) translateY(-50%)
+    display: flex
+    justify-content: center
+    align-items: center
+
+
+</style>
