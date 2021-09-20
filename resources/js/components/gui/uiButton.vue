8fd26f097ca099cc3679d1ebad0ad345be3ce563
iburangulov <iburanguloff@ya.ru>
2021-09-21 01:41:32 +0500

    ui items

---
+++ resources/js/components/gui/uiButton.vue
@@ -0,0 +1,15 @@
+<template>
+    <a class="waves-effect waves-light btn" @click="$emit('click')">
+        <slot/>
+    </a>
+</template>
+
+<script>
+export default {
+    name: "uiButton"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/gui/uiInput.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="row">
+        <div class="input-field col s12">
+            <input id="password" :type="type" class="validate">
+            <label for="password">{{ label }}</label>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "uiInput",
+    props: {
+        label: String,
+        type: {
+            type: String,
+            default: 'text',
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
