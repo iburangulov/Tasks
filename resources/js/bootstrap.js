65178ebd1a6cfa433bf42ae06e4f25329d21e8e2
iburangulov <iburanguloff@ya.ru>
2021-09-21 01:29:47 +0500

    materialize

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,3 +4,6 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+require('materialize-css/dist/js/materialize.min');
+require('materialize-css/dist/css/materialize.min.css');
+require('../fonts/materialize-icons.css');
